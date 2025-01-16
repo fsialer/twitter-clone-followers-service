2ed0c1f8098d8a48a626bace6e2cd65e596461b6
Fernando <asialer05@hotmail.com>
2025-01-16 10:03:04 -0500

    proyecto base

---
+++ src/main/java/com/fernando/ms/followers/app/twitter_clone_followers_service/TwitterCloneFollowersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.followers.app.twitter_clone_followers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneFollowersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneFollowersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/followers/app/twitter_clone_followers_service/TwitterCloneFollowersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.followers.app.twitter_clone_followers_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneFollowersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
